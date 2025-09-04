622bbff8c8677d18a368290a18b1916a30bf7450
AlejandroKashmir75 <156154628+AlejandroKashmir75@users.noreply.github.com>
2025-09-03 18:03:38 -0600

    fix: use relative import for MetricsService and import MetricsModule

--- src/risk/risk.module.ts
+++ src/risk/risk.module.ts
@@ -3,9 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RiskController } from './risk.controller';
 import { RiskService } from './risk.service';
 import { Risk } from './risk.entity';
-
+import { MetricsModule } from '../metrics/metrics.module';
 @Module({
-  imports: [TypeOrmModule.forFeature([Risk])],
+  imports: [TypeOrmModule.forFeature([Risk]),MetricsModule],
   controllers: [RiskController],
   providers: [RiskService],
   exports: [RiskService],
--- src/risk/risk.service.ts
+++ src/risk/risk.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Risk, Likelihood, Severity, RiskLevel } from './risk.entity';
 import { CreateRiskDto } from './dto/create-risk.dto';
-import { MetricsService } from 'src/metrics/metrics.service';
+import { MetricsService } from '../metrics/metrics.service';
 
 @Injectable()
 export class RiskService {
